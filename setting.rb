ac2ae91131d1939eb4e0365d632a6aba9c915729
muranet+git@gmail.com <muranet+git@gmail.com>
2021-01-24 18:12:00 +0900

    demo環境の追加

--- setting.rb
+++ setting.rb
@@ -3,6 +3,8 @@ ENV['TZ'] = 'Asia/Tokyo'
 case ENV['LAMBDA_ENV']
 when 'development'
   TABLE_NAME = 'Siisii.Timelines.develop'
+when 'demo'
+  TABLE_NAME = 'Siisii.Timelines.demo'
 when 'production'
   TABLE_NAME = 'Siisii.Timelines'
 else
